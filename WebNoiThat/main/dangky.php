30910710b1326c21480eed2fa5fc6c8b56e9b701
Duong <duonghai300804@gmail.com>
2025-04-23 22:00:51 +0700

    sua dang ki

--- WebNoiThat/main/dangky.php
+++ WebNoiThat/main/dangky.php
@@ -18,8 +18,8 @@
         $email = "";
 
 
-        include 'C:\xampp\htdocs\WebNoiThat\quanly\connect.php';  //Thêm file connect
-        include 'C:\xampp\htdocs\WebNoiThat\quanly\functions.php'; //File Hàm
+        include 'C:\xampp\htdocs\PhanMem\WebNoiThat\quanly\connect.php';  //Thêm file connect
+        include 'C:\xampp\htdocs\PhanMem\WebNoiThat\quanly\functions.php'; //File Hàm
 
         if (isset($_POST["btnDangKy"])) {
             $tentaikhoan = $_POST['txtTenTaiKhoan'];
